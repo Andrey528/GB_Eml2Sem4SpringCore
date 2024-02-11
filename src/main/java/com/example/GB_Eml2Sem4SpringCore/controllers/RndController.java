e6939dd8e008b493ee30f222bda176fa300b8ecd
Andrew <telepnev.1997@mail.ru>
2024-02-11 16:29:27 +0700

    add example of path and param data transfer

--- src/main/java/com/example/GB_Eml2Sem4SpringCore/controllers/RndController.java
+++ src/main/java/com/example/GB_Eml2Sem4SpringCore/controllers/RndController.java
@@ -3,15 +3,28 @@ package com.example.GB_Eml2Sem4SpringCore.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Random;
 
 @Controller
 public class RndController {
-    @GetMapping("/random")
-    public String getRngNumberPage(Model model) {
-        int randomInt = new Random().nextInt(1, 101);
+    @GetMapping("/random") // передача через параметры http://localhost:8080/random?min=20&max=80
+    public String getRngNumberPage(@RequestParam(name = "min") int min, @RequestParam(name = "max") int max, Model model) {
+        int randomInt = new Random().nextInt(min, max + 1);
         model.addAttribute("number", randomInt);
+        model.addAttribute("min", min);
+        model.addAttribute("max", max);
+        return "random";
+    }
+
+    @GetMapping("/random/{min}/{max}") // передача через путь http://localhost:8080/random/20/80
+    public String getFromPathRngNumberPage(@PathVariable(name = "min") int min, @PathVariable(name = "max") int max, Model model) {
+        int randomInt = new Random().nextInt(min, max + 1);
+        model.addAttribute("number", randomInt);
+        model.addAttribute("min", min);
+        model.addAttribute("max", max);
         return "random";
     }
 }
